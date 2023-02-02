165f0ae207f8a69fece72284e3587d44c8d21934
Guilherme Pereira <guilherme00pereira@gmail.com>
2023-02-02 12:41:09 -0300

    v_0.0.2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python project to convert images to WEBP
+
+version 0.0.2
--- process.py
+++ process.py
@@ -3,18 +3,24 @@ from PIL import Image
 
 path = "./images/"
 width = 1200
+extension = ".png"
 
-try:
-    for filename in os.listdir(path):
-        if filename.endswith(".png"):
-            print("lendo arquivo " + filename)
-            im = Image.open(os.path.join(path, filename))
-            aspect_ratio = im.height / im.width
-            height = int(width * aspect_ratio)
-            im = im.resize((width, height), Image.ANTIALIAS)
-            im.save(os.path.join(path, filename + "_resized.png"))
-            im2 = Image.open(os.path.join(path, filename + "_resized.png"))
-            im2.save(os.path.join(path, filename.replace(".png", ".webp")), "webp")
-        print("Images converted to WebP successfully!")
-except Exception as error:
-    print(error.args)
+def convert_to_webp():
+    try:
+        for filename in os.listdir(path):
+            if filename.endswith(extension):
+                print("lendo arquivo " + filename)
+                im = Image.open(os.path.join(path, filename))
+                aspect_ratio = im.height / im.width
+                height = int(width * aspect_ratio)
+                im = im.resize((width, height), Image.LANCZOS)
+                im.save(os.path.join(path, filename + "_resized" + extension))
+                im2 = Image.open(os.path.join(path, filename + "_resized" + extension))
+                im2.save(os.path.join(path, filename.replace(extension, ".webp")), "webp")
+            print("Images converted to WebP successfully!")
+    except Exception as error:
+        print(error.args)
+
+
+if __name__ == "__main__":
+    convert_to_webp()
